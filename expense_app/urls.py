e18084ced2a1b6a9278991adade5196b8bc22523
ClintonCode20 <clintonmatics@gmail.com>
2024-04-16 01:14:42 +0100

    first commit

---
+++ expense_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+    path("expenses/", views.list_expenses),
+    path("expenses/<int:pk>/", views.detail_expense),
+    path("categories/", views.list_categories)
+]
+
+# youtube version
+
+# expenses = http://127.0.0.1:8000/expenses/
+# categories = http://127.0.0.1:8000/categories/
+# del_expenses = http://127.0.0.1:8000/expenses/id/
